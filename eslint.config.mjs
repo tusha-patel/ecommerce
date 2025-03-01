a33860eda31912215e62de27cd483dda006afcb5
tusha-patel <tusha.b@queueloopsolutions.com>
2025-03-01 09:16:57 +0530

    esLint resolve

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  rules = {
-    "@typescript-eslint/no-explicit-any": "error"
+  {
+    "extends": ["next", "eslint:recommended"],
+    "rules": {
+      "react/no-unescaped-entities": "off"
+    }
   }
 
 ];
